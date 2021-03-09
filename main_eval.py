850928d2ebf75c2a14eb96ffc59d46ff0da6587a
Kenny Peng <knypng44@gmail.com>
2021-03-08 18:29:57 -0800

    Create and use new HDD-cached dataset class

--- main_eval.py
+++ main_eval.py
@@ -6,57 +6,61 @@ from matplotlib import pyplot as plt
 import database as db
 import neural
 
-with open('config.json', 'r') as infile:
-    config = json.load(infile)
-    CONFIG_TRANSFORMS = config['transforms']
+def main():
+    with open('config.json', 'r') as infile:
+        config = json.load(infile)
+        CONFIG_TRANSFORMS = config['transforms']
 
-eval_stocks = db.load_txt('./stock_lists/training_tickers.txt')
+    eval_stocks = db.load_txt('./stock_lists/training_tickers.txt')
 
-df = db.download_history(eval_stocks, cache_name='eval_stocks', start='2018-01-02', interval='1d')
+    df = db.download_history(eval_stocks, cache_name='eval_stocks', start='2018-01-02', interval='1d')
 
-df = db.apply_config_transforms(df, CONFIG_TRANSFORMS) # Adds more data (technicals, etc)
+    df = db.apply_config_transforms(df, CONFIG_TRANSFORMS) # Adds more data (technicals, etc)
 
-# Produces datasets each of the histories then samples randomly from them
-datasets = []
-for ticker in df.columns.levels[0]:
-    datasets.append(neural.historyDataset(df[ticker].dropna()))
-dataset = torch.utils.data.ConcatDataset(datasets)
-loader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=True, drop_last=True)
+    # Produces datasets each of the histories then samples randomly from them
+    datasets = []
+    for ticker in df.columns.levels[0]:
+        datasets.append(neural.historyDataset_cached(df[ticker].dropna(), './cache/historyDataset_eval_cache.h5' , ticker))
+    dataset = torch.utils.data.ConcatDataset(datasets)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=True, drop_last=True)
 
-state = torch.load('model.pt')
-n_input = state['n_input']
-model = neural.LSTM(n_input) # Resets model with a batch size of target list length
-model.load_state_dict(state['model'])
-model.eval()
-print('LSTM loaded')
+    state = torch.load('model.pt')
+    n_input = state['n_input']
+    model = neural.LSTM(n_input) # Resets model with a batch size of target list length
+    model.load_state_dict(state['model'])
+    model.eval()
+    print('LSTM loaded')
 
-data, label = next(iter(loader))
-data = data.float()
+    data, label = next(iter(loader))
+    data = data.float()
 
-out_norm = model(data).detach().view(-1).numpy()
+    out_norm = model(data).detach().view(-1).numpy()
 
-last = data[:, -1, 0].numpy()
-label = label.view(-1).numpy()
+    last = data[:, -1, 0].numpy()
+    label = label.view(-1).numpy()
 
-dx_label = label-last
-dx_norm = out_norm-last
+    dx_label = label-last
+    dx_norm = out_norm-last
 
-accuracy_norm = (np.sum(np.logical_and(dx_label < 0, dx_norm < 0))+np.sum(np.logical_and(dx_label > 0, dx_norm > 0))) / 100
-print(f'Accuracy of model: {accuracy_norm:.3f}')
+    accuracy_norm = (np.sum(np.logical_and(dx_label < 0, dx_norm < 0))+np.sum(np.logical_and(dx_label > 0, dx_norm > 0))) / 100
+    print(f'Accuracy of model: {accuracy_norm:.3f}')
 
-accuracy_alwaysup = np.sum(np.sum(np.logical_and(dx_label > 0, 1)))/100
-print(f'Accuracy of always predicting up: {accuracy_alwaysup:.3f}')
+    accuracy_alwaysup = np.sum(np.sum(np.logical_and(dx_label > 0, 1)))/100
+    print(f'Accuracy of always predicting up: {accuracy_alwaysup:.3f}')
 
-rng = np.random.default_rng()
-random = rng.integers(2, size=100)
-accuracy_random = (np.sum(np.logical_and(dx_label < 0, random))+np.sum(np.logical_and(dx_label > 0, random))) / 100
-print(f'Accuracy of always predicting randomly: {accuracy_random:.3f}')
+    rng = np.random.default_rng()
+    random = rng.integers(2, size=100)
+    accuracy_random = (np.sum(np.logical_and(dx_label < 0, random))+np.sum(np.logical_and(dx_label > 0, random))) / 100
+    print(f'Accuracy of always predicting randomly: {accuracy_random:.3f}')
 
-p = np.argsort(dx_label)
-dx_label = dx_label[p]
-dx_norm = dx_norm[p]
+    p = np.argsort(dx_label)
+    dx_label = dx_label[p]
+    dx_norm = dx_norm[p]
 
-plt.plot(dx_label, '.r')
-plt.plot(dx_norm, '.g')
-plt.plot(np.zeros(100))
-plt.show()
+    plt.plot(dx_label, '.r')
+    plt.plot(dx_norm, '.g')
+    plt.plot(np.zeros(100))
+    plt.show()
+
+if __name__ == '__main__':
+    main()
--- neural.py
+++ neural.py
@@ -1,4 +1,7 @@
 import json
+import multiprocessing as mp
+import numpy as np
+import tables
 import torch
 
 import database as db
@@ -62,6 +65,87 @@ class historyDataset(torch.utils.data.Dataset):
         
         return input_data, label
 
+# Switching from the normalization scheme below may require deleting your previous cache!
+def _process_arr_handle(workQueue, writeQueue):
+    while True:
+        idx, arr_idx = workQueue.get()
+
+        mask = np.ones((arr_idx.shape), dtype=bool)
+        mask[-1] = False
+        # Normalize while supressing divide by zero (prices don't change over many days sometimes)
+        with np.errstate(divide='ignore', invalid='ignore'):
+            arr_max = np.amax(arr_idx, axis=0, keepdims=True, initial=0, where=mask)
+            arr_min = np.amin(arr_idx, axis=0, keepdims=True, initial=0, where=mask)
+            arr_idx = (arr_idx-arr_min) / (arr_max-arr_min)
+        np.nan_to_num(arr_idx, copy=False, nan=0, posinf=0, neginf=0)
+
+        writeQueue.put((idx, arr_idx))
+
+class historyDataset_cached(torch.utils.data.Dataset):
+    def __init__(self, df, path, id : str):
+        self.len = len(df.index)-TOTAL_INTERVAL_SIZE+1
+        if self.len <= 0:
+            self.len = 0 # If a dataset is invalid, this prevents an error and omits the dataset
+            return
+        
+        self.path = path
+        self.id = id
+        self.example_shape = (TOTAL_INTERVAL_SIZE, len(df.columns))
+        self.close_index = list(df.columns).index('Close')
+        
+        params = {
+            'example_shape': self.example_shape,
+            'num_samples': self.len
+        }
+        h5file = tables.open_file(self.path, 'a')
+        try:
+            # Raises tables.exceptions.NodeError if already exists
+            atom = tables.Atom.from_dtype(np.dtype((np.float32, self.example_shape)))
+            h5arr = h5file.create_earray('/', self.id, atom, (0,), expectedrows=self.len)
+
+            workers = []
+            workQueue = mp.Queue()
+            writeQueue = mp.Queue()
+            for idx in range(NUM_WORKERS):
+                worker = mp.Process(target=_process_arr_handle, args=(workQueue, writeQueue))
+                worker.start()
+                workers.append(worker)
+            
+            arr = df.values.astype('float32') # Reduce to 32-bit float to save HDD space (TODO: consider 16-bit?)
+            items_remaining = self.len
+            for idx in range(self.len):
+                arr_idx = arr[idx:idx+TOTAL_INTERVAL_SIZE].copy() # Require deep copy to not modify original array
+                workQueue.put((idx, arr_idx))
+            while items_remaining:
+                idx, arr_idx = writeQueue.get() # TODO: Currently doesn't store results in order
+                arr_dix = np.expand_dims(arr_idx, 0) # Prepares arr_idx to be appended to h5arr along dim 0
+                h5arr.append(arr_dix)
+                items_remaining -= 1
+
+            for idx in range(NUM_WORKERS):
+                workers[idx].terminate()
+                workers[idx].join()
+            workQueue.close()
+            writeQueue.close()
+
+            h5file.set_node_attr('/'+self.id, 'params', params)
+        except tables.exceptions.NodeError: # In case the node already exists, reuse it...
+            # but throw an error if the cache was made with different settings
+            if h5file.get_node_attr('/'+self.id, 'params') != params:
+                raise ValueError(f'{self.id} cache params do not match current settings')
+        h5file.close()
+    def __len__(self):
+        return self.len
+    def __getitem__(self, idx):
+        h5file = tables.open_file(self.path, 'r')
+
+        arr_idx = h5file.get_node('/', self.id)[idx]
+        input_data = arr_idx[0:INTERVAL_TENSOR_SIZE]
+        label = arr_idx[INTERVAL_TENSOR_SIZE][self.close_index]
+
+        h5file.close()
+        return input_data, label
+
 def generate_split_loaders(df, shuffle, batch_size, **kwargs):
     datasets_training = []
     datasets_validation = []
@@ -73,8 +157,8 @@ def generate_split_loaders(df, shuffle, batch_size, **kwargs):
         
         df_t_training, df_t_validation = db.split(df_t, **kwargs)
 
-        datasets_training.append(historyDataset(df_t_training))
-        datasets_validation.append(historyDataset(df_t_validation))
+        datasets_training.append(historyDataset_cached(df_t_training, './cache/historyDataset_train_cache.h5', foo))
+        datasets_validation.append(historyDataset_cached(df_t_validation, './cache/historyDataset_valid_cache.h5', foo))
     
     dataset_training = torch.utils.data.ConcatDataset(datasets_training)
     loader_training = torch.utils.data.DataLoader(dataset_training, batch_size,
